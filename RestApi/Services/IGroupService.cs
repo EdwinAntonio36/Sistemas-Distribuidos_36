31b947c8283680f2769e525a6facb3b3505d9e28
EdwinAntonio36 <134561165+EdwinAntonio36@users.noreply.github.com>
2024-09-17 22:57:56 -0600

    RestApi GetGroupByName
    Feat: Se agregaron los cambios para el metodo de GetGroupBynAME

--- RestApi/Services/IGroupService.cs
+++ RestApi/Services/IGroupService.cs
@@ -4,4 +4,6 @@ namespace RestApi.Services;
 
 public interface IGroupService {
     Task<GroupUserModel> GetGroupByIdAsync (string id, CancellationToken cancellationToken);
+
+    Task<List<GroupUserModel>> GetGroupByNameAsync(string name, CancellationToken cancellationToken);
 }
